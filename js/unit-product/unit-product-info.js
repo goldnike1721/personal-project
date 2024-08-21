04c744cb3ec865b1a4345a18edbb917a45b6f831
goldnike1721 <petro.rozumiei@gmail.com>
2024-08-21 04:38:12 +0300

    Add: styles css for the page 'product'

--- js/unit-product/unit-product-info.js
+++ js/unit-product/unit-product-info.js
@@ -6,24 +6,33 @@
         productContainer.innerHTML = `
         <section class="products">
             <div class="products-container">
-                <div class="products-container__top">
-                    <div class="products__top-title">
-                        <a class="products-top__title-link" href="shop.html">
-                            <h2 class="products-top__title-text">Store homepage</h2>
-                        </a>
-                    </div>
-                </div>
-                <div class="products-container__contents">
-                    <div class="products-contents__carousel">
-                        <div class="products-contents__carousel-image">
-                            <img class="products-carousel__image" width="150" src="${product.image}"
-                                alt="${product.altText}">
+                <div class="product-general-container">
+                    <div class="products-container__top">
+                        <div class="products__top-title">
+                            <a class="products-top__title-link" href="shop.html">
+                                <h2 class="products-top__title-text">Store homepage</h2>
+                            </a>
                         </div>
                     </div>
-                    <div class="products-contents__bottom-image">
-                        <div class="products-bottom__image">
-                            <img class="products-additionally__image" width="50" src="${product.smallimage}"
-                                alt="${product.altText}">
+                    <div class="products-container__contents">
+                        <div class="products-contents__carousel">
+                            <div class="products-contents__carousel-image">
+                                <img class="products-carousel__image" src="${product.image}" alt="${product.altText}">
+                            </div>
+                        </div>
+                        <div class="products-contents__bottom-image">
+                            <div class="products-bottom__image">
+                                <img class="products-additionally__image" src="${product.smallimage}" alt="${product.altText}">
+                            </div>
+                            <div class="products-bottom__image">
+                                <img class="products-additionally__image" src="${product.smallimage}" alt="${product.altText}">
+                            </div>
+                            <div class="products-bottom__image">
+                                <img class="products-additionally__image" src="${product.smallimage}" alt="${product.altText}">
+                            </div>
+                            <div class="products-bottom__image">
+                                <img class="products-additionally__image" src="${product.smallimage}" alt="${product.altText}">
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -48,54 +57,37 @@
                                     <input type="number" id="quantity" name="quantity" min="1" value="1">
                                 </form>
                             </div>
-                            <div class="products-description__button">
-                                <button class="products-description__button-cart" onclick="addToCart('${product.id}')">Add to
+                            <div class="products-description__button-cart">
+                                <button class="products-description__cart" onclick="addToCart('${product.id}')">Add to
                                     Cart</button>
                             </div>
                         </div>
                     </div>
-                    <div class="products-description__bottom">
-                        <div class="products-description__bottom">
-                            <div class="products-description__bottom-content">
-                                <div class="products-description__button">
-                                    <span class="products-description__tab-button active"
-                                        onclick="openTab(event, 'products-description__description')">DESCRIPTION</span>
-                                </div>
-                                <div class="products-description__button">
-                                    <span class="products-description__tab-button"
-                                        onclick="openTab(event, 'products-description__usage')">USAGE</span>
-                                </div>
-                                <div class="products-description__button">
-                                    <span class="products-description__tab-button"
-                                        onclick="openTab(event, 'products-description__ingredients')">INGREDIENTS</span>
-                                </div>
+                   <div class="products-description__tab-container">
+                        <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
+                            <input type="radio" class="btn-check" name="btnradio" id="btnradio1" autocomplete="off" checked>
+                            <label class="btn btn-outline-primary" for="btnradio1">DESCRIPTION</label>
+
+                            <input type="radio" class="btn-check" name="btnradio" id="btnradio2" autocomplete="off">
+                            <label class="btn btn-outline-primary" for="btnradio2">USAGE</label>
+
+                            <input type="radio" class="btn-check" name="btnradio" id="btnradio3" autocomplete="off">
+                            <label class="btn btn-outline-primary" for="btnradio3">INGREDIENTS</label>
+                        </div>
+
+                        <div id="products-description__description" class="products-description__tab products-description__tab-active">
+                            <div class="products-description__composition">
+                                <p class="products-description__composition-text">${product.description}</p>
                             </div>
-                            <div class="products-description__tab-container">
-                                <div id="products-description__description"
-                                    class="products-description__tab products-description__tab-active">
-                                    <div class="products-description__title">
-                                        <p class="products-description__title-description">DESCRIPTION</p>
-                                    </div>
-                                    <div class="products-description__composition">
-                                        <p class="products-description__composition-description">${product.description}</p>
-                                    </div>
-                                </div>
-                                <div id="products-description__usage" class="products-description__tab">
-                                    <div class="products-description__title">
-                                        <p class="products-description__title-usage">USAGE</p>
-                                    </div>
-                                    <div class="products-description__composition">
-                                        <p class="products-description__composition-usage">${product.usage}</p>
-                                    </div>
-                                </div>
-                                <div id="products-description__ingredients" class="products-description__tab">
-                                    <div class="products-description__title">
-                                        <p class="products-description__title-ingredients">INGREDIENTS</p>
-                                    </div>
-                                    <div class="products-description__composition">
-                                        <p class="products-description__composition-ingredients">${product.ingredients}</p>
-                                    </div>
-                                </div>
+                        </div>
+                        <div id="products-description__usage" class="products-description__tab-1">
+                            <div class="products-description__composition">
+                                <p class="products-description__composition-text">${product.usage}</p>
+                            </div>
+                        </div>
+                        <div id="products-description__ingredients" class="products-description__tab-1">
+                            <div class="products-description__composition">
+                                <p class="products-description__composition-text">${product.ingredients}</p>
                             </div>
                         </div>
                     </div>
